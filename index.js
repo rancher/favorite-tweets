6aa334687b51ae856b03738956d889327b8d3011
Vincent Fiduccia <vincent@rancher.com>
2019-01-29 11:38:02 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,134 @@
+const http = require('http');
+const request = require('request');
+const PORT = process.env.PORT || 80;
+const USERNAME = process.env.USERNAME || 'Rancher_Labs';
+const CONSUMER = process.env.TWITTER_CONSUMER;
+const SECRET = process.env.TWITTER_SECRET;
+let lastToken;
+
+
+if ( !USERNAME ) {
+  console.error('Must specify USERNAME in environment');
+  process.exit(1);
+}
+
+if ( !CONSUMER ) {
+  console.error('Must specify TWITTER_CONSUMER in environment');
+  process.exit(1);
+}
+
+if ( !SECRET ) {
+  console.error('Must specify TWITTER_SECRET in environment');
+  process.exit(1);
+}
+
+getToken(function(err) {
+  if ( err ) {
+    console.error(err);
+    process.exit(1);
+  }
+});
+
+const server = http.createServer(handle);
+server.listen(PORT, function(err) {
+  if ( err ) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    console.log(`Listening on ${PORT}`);
+  }
+});
+
+function handle(req, res, retry=true) {
+  getTweets(lastToken, function(err, tweets) {
+    if ( err ) {
+      if ( retry ) {
+        console.log(err);
+        getToken(function(err) {
+          if (err) {
+            return respondError(req, res, err);
+          }
+
+          handle(req, res, false);
+        });
+      } else {
+        return respondError(req, res, err);
+      }
+    } else {
+      return respondSuccess(req, res, tweets);
+    }
+  });
+}
+
+function respondSuccess(req, res, data) {
+  res.setHeader('Content-Type','application/json');
+  res.statusCode = 200;
+  const out = JSON.stringify(data, null, 2);
+  res.end(out);
+  console.log(`Sent ${out.length} bytes to ${req.socket.localAddress}`);
+}
+
+function respondError(req, res, err) {
+  const obj = {
+    type: 'error',
+    status: 500,
+    message: err,
+  };
+
+  res.setHeader('Content-Type','application/json');
+  res.statusCode = 500;
+  res.end(JSON.stringify(obj, null, 2));
+  console.error(err);
+}
+
+function getToken(cb) {
+  request.post('https://api.twitter.com/oauth2/token', {
+    auth: {
+      username: CONSUMER,
+      password: SECRET,
+    },
+    form: {
+      'grant_type': 'client_credentials',
+    }
+  }, function(err, res, body) {
+    if ( err ) {
+      return cb(err);
+    } else if ( res.statusCode !== 200 ) {
+      return cb(body);
+    }else {
+      let obj;
+      try {
+        const obj = JSON.parse(body);
+        const token = obj.access_token;
+        lastToken = token;
+        console.log('Got new token');
+
+        return cb();
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  });
+}
+
+function getTweets(token, cb) {
+  request.get(`https://api.twitter.com/1.1/favorites/list.json?count=12&screen_name=${USERNAME}`, {
+    auth: {
+      bearer: token
+    }
+  }, function(err, res, body) {
+    if ( err ) {
+      return cb(err);
+    } else if ( res.statusCode !== 200 ) {
+      return cb(null, body);
+    } else {
+      try {
+        const obj = JSON.parse(body);
+        console.log('Got new tweets');
+        return cb(null, obj);
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  });
+}
